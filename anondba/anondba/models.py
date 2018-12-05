add0ccc17cadad6d401744100bf34496b57622d9
yaohongdi <yaokevin1987@gamil.com>
2018-12-05 14:56:52 +0800

    3

--- anondba/anondba/models.py
+++ anondba/anondba/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column,String,Integer
-
+from sqlalchemy import Date
 engine=create_engine('mysql+mysqldb://root@localhost:3306/anondba?charset=utf8')
 Base=declarative_base()
 
@@ -12,6 +12,18 @@ class Course(Base):
     name = Column(String(64),index=True)
     desc=Column(String(1024))
     type1=Column(String(64),index=True)
+class User(Base):
+    __tablename__ ='users'
+
+    id = Column(Integer,primary_key=True)
+    name = Column(String(64),index=True)
+    type=Column(String(64),index=True)
+    status=Column(String(64),index=True)
+    job=Column(String(64))
+    level=Column(Integer,index=True)
+    join_date=Column(Date)
+    learn_courses_num=Column(Integer)
+
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
