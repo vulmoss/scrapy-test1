9d4a4cbd858c4a0b3e1e5c818f4de093223b5850
yaohongdi <yaokevin1987@gamil.com>
2018-12-05 14:33:41 +0800

    the second commit

--- anondba/anondba/pipelines.py
+++ anondba/anondba/pipelines.py
@@ -4,8 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+from sqlalchemy.orm import sessionmaker
+from anondba.models import Course,engine
 
 class AnondbaPipeline(object):
     def process_item(self, item, spider):
+        item['name'] = int(item['name'])
+        self.session.add(Course(**item))
         return item
+    def open_spider(self,spider):
+        Session=sessionmaker(bind=engine)
+        self.session=Session()
+    def close_spider(self,spider):
+        self.session.commit()
+        self.session.close()
